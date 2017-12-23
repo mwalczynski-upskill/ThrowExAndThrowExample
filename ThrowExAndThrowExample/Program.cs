81f36a80cf3e420705a03e16cf911f9a24f6f9de
Michal Walczynski <mwalczynski@pgs-soft.com>
2017-12-23 18:03:57 +0100

    Add project files.

---
+++ ThrowExAndThrowExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ThrowExAndThrowExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

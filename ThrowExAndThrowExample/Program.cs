f30acd6e4a4bb89af1c62b53094a509ca86f0d13
Michal Walczynski <mwalczynski@pgs-soft.com>
2017-12-23 19:43:20 +0100

    Throw vs Throw ex

--- ThrowExAndThrowExample/Program.cs
+++ ThrowExAndThrowExample/Program.cs
@@ -6,7 +6,62 @@ namespace ThrowExAndThrowExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Throw();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Throw");
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            try
+            {
+                ThrowEx();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Throw Ex");
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void ThrowEx()
+        {
+            try
+            {
+                DeivideByZero();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static void Throw()
+        {
+            try
+            {
+                DeivideByZero();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public static void DeivideByZero()
+        {
+            FinallyDivideByZero();
+        }
+
+        public static void FinallyDivideByZero()
+        {
+            int x = 0;
+            var y = 1 / x;
         }
     }
 }
